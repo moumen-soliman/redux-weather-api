69f4a687c217983715fd78c38beaeb5cf4003c41
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-02 23:18:40 +0200

    Change country

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ const ROOT_URL = `http://openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-	const url = `${ROOT_URL}&q=${city},us`;
+	const url = `${ROOT_URL}&q=${city},eg`;
 	const request = axios.get(url);
 
 	console.log('Request:', request);
