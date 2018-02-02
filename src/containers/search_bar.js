4e50f833b3e4527bf64b45aa9a587e84c4f5c57a
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-02 22:50:04 +0200

    fix more one than export module err

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 
