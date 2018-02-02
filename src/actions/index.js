8178dbee213b326b8facb3c30889c158f844b848
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-02 23:03:26 +0200

    Avoiding State Mutations in reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
 const API_KEY = 'b6907d289e10d714a6e88b30761fae22';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://sample.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
 	const url = `${ROOT_URL}&q=${city},us`;
 	const request = axios.get(url);
+
+	console.log('Request:', request);
 
 	return {
 		type: 'FETCH_WEATHER',
