55a1d5a4de917d1fcfa42a10d3f52a377fd3af02
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-02 23:11:20 +0200

    fix undefined city

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'b6907d289e10d714a6e88b30761fae22';
-const ROOT_URL = `http://sample.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
