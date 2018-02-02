0a1375a865361359f53674b82a2cf07ab2e105c3
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-02 22:41:19 +0200

    configure with axios

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
 export default class SearchBar extends Component {
 	constructor(props) {
@@ -7,6 +10,7 @@ export default class SearchBar extends Component {
 		this.state = { term: '' };
 
 		this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
 	}
 
 	onInputChange(e) {
@@ -15,6 +19,9 @@ export default class SearchBar extends Component {
 
 	onFormSubmit(e) {
 		e.preventDefault();
+
+		this.props.fetchWeather(this.state.term);
+		this.setState({ term:'' });
 	}
 
 	render() {
@@ -33,3 +40,9 @@ export default class SearchBar extends Component {
 		);
 	}
 }
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
