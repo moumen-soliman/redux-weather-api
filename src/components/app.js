fc52cf07e1f16a3b08371f83304da7c3ec44b5b7
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-03 00:27:04 +0200

    Add (temps, pressures, humidities) with their units and view

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
+		<h3>Please Search in egyptian countries</h3>
+		<span>examples: Cairo, Giza</span>
       	<SearchBar />
       	<WeatherList />
       </div>
